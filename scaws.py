41307a882848ef0fda274acc8627ee355b5ea3da
delete <renedo@gmail.com>
2015-10-14 18:43:33 +0200

    now menu bases on urwid

--- scaws.py
+++ scaws.py
@@ -1,119 +1,134 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-scaws (Simple Console for AWS) is, as the names says, a text based console for AWS
+scaws (Simple Console for AWS) is a text based console for AWS
 """
 
-import boto3
-import sys
-import os
 
+import pdb
+import os
+import ec2
+import urwid
 
 
 __author__ = "Iván Renedo"
 __copyright__ = "GPL"
 
 
-# aws_services is used for list supported aws services and options by scaws
-aws_services = (
-    ('EC2 - Elastic Cloud Computing',
-         ('Instances',
-            ('List Instances',
-             'Stop Instances',
-             'Start Instances',
-            ),
-          'Images',
-          'EBS - Elastic Block Store',
-          'Network And Security',
-          'Load Balancing',
-          'Auto Scaling')),
-    ('S3 - Simple Storage Service',))
+def menu_button(caption, callback):
+
+    button = urwid.Button(caption)
+    urwid.connect_signal(button, 'click', callback)
+    return urwid.AttrMap(button, None, focus_map='reversed')
 
 
-def main_menu(services):
-    """
-    Draws the main menu
-    """
-    while True:
-        if os.name == 'nt':
-            os.system('cls')
+def sub_menu(caption, choices):
+    contents = menu(caption, choices)
+
+    def open_menu(button):
+        return top.open_box(contents)
+    return menu_button([caption, u'...'], open_menu)
+
+
+def menu(title, choices):
+    body = [urwid.Text(('banner',title), align='center'), urwid.Divider()]
+    body.extend(choices)
+    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+
+def item_chosen(button):
+    response = urwid.Text([u'You chose ', button.label, u'\n'])
+    done = menu_button(u'Ok', exit_program)
+    top.open_box(urwid.Filler(urwid.Pile([response, done])))
+
+
+def exit_program(button):
+    raise urwid.ExitMainLoop()
+
+
+def divider():
+    return urwid.Divider()
+
+
+class boxes(urwid.WidgetPlaceholder):
+    max_box_levels = 5
+
+    def __init__(self, box):
+        super(boxes, self).__init__(urwid.SolidFill(u' '))
+        self.box_level = 0
+        self.open_box(box)
+
+    def open_box(self, box):
+        self.original_widget = urwid.Overlay(urwid.LineBox(box),
+            self.original_widget,
+            align='center', width=('relative', 80),
+            valign='middle', height=('relative', 80),
+            min_width=24, min_height=8)
+        self.box_level += 1
+
+    def keypress(self, size, key):
+        if key == 'esc' and self.box_level > 1:
+            self.original_widget = self.original_widget[0]
+            self.box_level -= 1
         else:
-            os.system('clear')
+            return super(boxes, self).keypress(size, key)
 
-        rightoptions = [0]
-        title = 'AWS Services'
-        tab = "         "
-        print(tab + '-' * len(title))
-        print(tab + title)
-        print(tab + '-' * len(title))
-        option = 1
-        for ser in services:
-            print("[{0}] {1}".format(option, ser[0]))
-            rightoptions.append(option)
-            option += 1
 
-        print("\n[0] Exit")
-        chosen = input('\nChose Option->')
-        # check for a correct option
-        if chosen in rightoptions:
-            break
+def item_back(button):
+    top.original_widget = top.original_widget[0]
+    top.box_level -= 1
 
-    return chosen
 
-def ec2_menu(elem):
-    """
-    Draws the ec2 menu
-    """
-    while True:
-        if os.name == 'nt':
-            os.system('cls')
-        else:
-            os.system('clear')
+"""
+MAIN
+"""
 
-        rightoptions = [0]
-        title = 'EC2 Services'
-        tab = "         "
-        print(tab + '-' * len(title))
-        print(tab + title)
-        print(tab + '-' * len(title))
-        option = 1
-        for ser in elem[1]:
-            print("[{0}] {1}".format(option, ser))
-            rightoptions.append(option)
-            option += 1
 
-        print("\n[0] Back")
-        chosen = input('\nChose Option->')
-        # check for a correct option
-        if chosen in rightoptions:
-            break
+menu_top = menu(u'[[  SCAWS Main Menu  ]]', [
+    sub_menu(u'EC2 - Elastic Cloud Computing', [
+        sub_menu(u'Instances', [
+            menu_button(u'Describe Instances', item_chosen),
+            divider(),
+            menu_button(u'Back', item_back),
+        ]),
+        sub_menu(u'Images (AMIs)', [
+            divider(),
+            menu_button(u'Back', item_chosen),
+        ]),
+        sub_menu(u'Elastic Bloc Store (EBS)', [
+            divider(),
+            menu_button(u'Back', item_back),
+        ]),
+        sub_menu(u'NetWork & Security', [
+            divider(),
+            menu_button(u'Back', item_chosen),
+        ]),
+        sub_menu(u'Load Balancing', [
+            divider(),
+            menu_button(u'Back', item_back)
+        ]),
+        sub_menu(u'AutoScaling', [
+            divider(),
+            menu_button(u'Back', item_chosen),
+        ]),
+        divider(),
+        menu_button(u'Back', item_back)
+    ]),
+    sub_menu(u'S3 - Simple Storage Service', [
+        sub_menu(u'Preferences', [
+            menu_button(u'Appearance', item_chosen),
+        ]),
+        menu_button(u'Lock Screen', item_chosen),
+    ]),
+    divider(),
+    menu_button(u'Exit', exit_program)
+])
 
-    return chosen
+top = boxes(menu_top)
+urwid.MainLoop(top, palette=[('banner', 'white', 'black'), ('reversed', 'standout', '')]).run()
 
-def ec2(elements):
-    """
-    Draws the EC2 menu
-    """
-    ec2_menu(elements)
-    if service == 0:
-        return 0
-    if service == 1:
-        print("servicio de EC2")
-    return 0
 
-def s3(elements):
-    """
-    Draws the s3 menu
-    """
-    return 0
-
-while True:
-    service = main_menu(aws_services)
-
-    if service == 0:
-        sys.exit(0)
-    if service == 1:
-        ec2(aws_services[0])
-    if service == 2:
-        s3(aws_services[1])
-
+if os.name == 'nt':
+    os.system('cls')
+else:
+    os.system('clear')

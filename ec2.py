19dda6732b4a63cbeafbb828896b5261f1cb8bc3
delete <renedo@gmail.com>
2015-10-17 10:50:30 +0200

    urwid menus almost working

--- ec2.py
+++ ec2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import urwid
 import boto3 as aws
 
 """
@@ -7,53 +8,14 @@ EC2 related functions
 """
 
 
-def instances():
-    """
-    Instances Menu
-    """
-
-    while True:
-        if os.name == 'nt':
-            os.system('cls')
-        else:
-            os.system('clear')
-        tab = "         "
-
-        title = "[[  Instances  ]]"
-        print(tab + '-' * len(title))
-        print(tab + title)
-        print(tab + '-' * len(title))
-        print("")
-        print("[1] :: List Instances")
-
-        print("\n[0] Back")
-
-        # check for a correct option
-        try:
-            chosen = input('\nChose Option->')
-            # check for a correct option
-            if chosen in range(2):
-                if chosen == 0:
-                    break
-                elif chosen == 1:
-                    list_instances()
-        except SyntaxError:
-            # non common characters  -> redraw menu
-            pass
-        except NameError:
-            # non numerical option -> redraw menu
-            pass
-    return 0
-
-
 def list_instances():
-    """
-    List instances in aws account
-    :return: nothing
-    """
+
     ec2 = aws.client('ec2')
     reservations = ec2.describe_instances()
+
     inst = [res['Instances'][0] for res in reservations['Reservations']]
-    for inst in inst:
-        print("InstanceId : {0}".format(inst['InstanceId']))
-    wait = input("Press any key to continue")
+
+    return inst
+
+
+
--- scaws.py
+++ scaws.py
@@ -15,120 +15,149 @@ __author__ = "Iván Renedo"
 __copyright__ = "GPL"
 
 
-def menu_button(caption, callback):
+def draw_menu(title, menu_body, footer):
+    body = [urwid.Divider()]
+    for x in menu_body:
+        button = urwid.Button(x[0])
+        if x[0] == 'Exit':
+            body.append(urwid.Divider())
+        urwid.connect_signal(button, 'click', x[1])
+        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
 
-    button = urwid.Button(caption)
-    urwid.connect_signal(button, 'click', callback)
-    return urwid.AttrMap(button, None, focus_map='reversed')
-
-
-def sub_menu(caption, choices):
-    contents = menu(caption, choices)
-
-    def open_menu(button):
-        return top.open_box(contents)
-    return menu_button([caption, u'...'], open_menu)
-
-
-def menu(title, choices):
-    body = [urwid.Text(('banner',title), align='center'), urwid.Divider()]
-    body.extend(choices)
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+    main_loop.widget.body = urwid.Overlay(urwid.ListBox(urwid.SimpleFocusListWalker(body)),
+                         urwid.SolidFill(u' '), align='center', width=('relative', 50),
+                         valign='middle', height=('relative', 50),)
+    main_loop.widget.footer = footer
+    main_loop.widget.title = urwid.AttrMap(urwid.Text(title, align='center'), 'titlebar')
+    main_loop.draw_screen()
 
 
-def item_chosen(button):
-    response = urwid.Text([u'You chose ', button.label, u'\n'])
-    done = menu_button(u'Ok', exit_program)
-    top.open_box(urwid.Filler(urwid.Pile([response, done])))
+def draw_result(title, body, footer):
+    main_loop.widget.body = body
+    main_loop.widget.title = urwid.AttrMap(urwid.Text(title, align='center'), 'titlebar')
+    main_loop.widget.footer = footer
+    main_loop.draw_screen()
 
 
-def exit_program(button):
+def exit_program(key):
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
     raise urwid.ExitMainLoop()
 
 
-def divider():
-    return urwid.Divider()
-
-
-class boxes(urwid.WidgetPlaceholder):
-    max_box_levels = 5
-
-    def __init__(self, box):
-        super(boxes, self).__init__(urwid.SolidFill(u' '))
-        self.box_level = 0
-        self.open_box(box)
-
-    def open_box(self, box):
-        self.original_widget = urwid.Overlay(urwid.LineBox(box),
-            self.original_widget,
-            align='center', width=('relative', 80),
-            valign='middle', height=('relative', 80),
-            min_width=24, min_height=8)
-        self.box_level += 1
-
-    def keypress(self, size, key):
-        if key == 'esc' and self.box_level > 1:
-            self.original_widget = self.original_widget[0]
-            self.box_level -= 1
+def handle_input(key):
+    if key in ['Q', 'q']:
+        if os.name == 'nt':
+            os.system('cls')
         else:
-            return super(boxes, self).keypress(size, key)
+            os.system('clear')
+        raise urwid.ExitMainLoop()
+    if key in ['b','B']:
+        menu_main(None)
 
 
-def item_back(button):
-    top.original_widget = top.original_widget[0]
-    top.box_level -= 1
+def menu_main(button):
+
+    menu_body = [
+        (u'EC2 - Elastic Cloud Computing', menu_ec2),
+        (u'S3 - Simple Storage Service', menu_s3),
+        (u'Exit', exit_program)
+        ]
+
+    title = u'SCAWS Main Menu'
+
+    footer = urwid.Text([u'Press (', ('quit button', u'Q'), u') to quit.'])
+
+    draw_menu(title,menu_body, footer)
 
 
-"""
-MAIN
-"""
+def menu_ec2(button):
+
+    menu_body = [
+        (u'Instances', ec2_instances),
+        (u'Images (AMIs)', exit_program),
+        (u'Elastic Bloc Store (EBS)', exit_program),
+        (u'NetWork & Security', exit_program),
+        (u'Load Balancing', exit_program),
+        (u'AutoScaling', exit_program),
+        (u'Back', menu_main)
+        ]
+    title = u'EC2 - Elastic Cloud Computing'
+
+    footer = urwid.Text([u'Press (', ('quit button', u'Q'), u') to quit.'])
+
+    draw_menu(title, menu_body, footer)
 
 
-menu_top = menu(u'[[  SCAWS Main Menu  ]]', [
-    sub_menu(u'EC2 - Elastic Cloud Computing', [
-        sub_menu(u'Instances', [
-            menu_button(u'Describe Instances', item_chosen),
-            divider(),
-            menu_button(u'Back', item_back),
-        ]),
-        sub_menu(u'Images (AMIs)', [
-            divider(),
-            menu_button(u'Back', item_chosen),
-        ]),
-        sub_menu(u'Elastic Bloc Store (EBS)', [
-            divider(),
-            menu_button(u'Back', item_back),
-        ]),
-        sub_menu(u'NetWork & Security', [
-            divider(),
-            menu_button(u'Back', item_chosen),
-        ]),
-        sub_menu(u'Load Balancing', [
-            divider(),
-            menu_button(u'Back', item_back)
-        ]),
-        sub_menu(u'AutoScaling', [
-            divider(),
-            menu_button(u'Back', item_chosen),
-        ]),
-        divider(),
-        menu_button(u'Back', item_back)
-    ]),
-    sub_menu(u'S3 - Simple Storage Service', [
-        sub_menu(u'Preferences', [
-            menu_button(u'Appearance', item_chosen),
-        ]),
-        menu_button(u'Lock Screen', item_chosen),
-    ]),
-    divider(),
-    menu_button(u'Exit', exit_program)
-])
+def ec2_instances(button):
+    menu_body = [
+        (u'List Instances', describe_instances),
+        (u'Start Instances', exit_program),
+        (u'Stop Instances', exit_program),
+        (u'Back', menu_ec2)
+        ]
+    title = u'Instances'
+    footer = urwid.Text([u'Press (', ('quit button', u'Q'), u') to quit.'])
 
-top = boxes(menu_top)
-urwid.MainLoop(top, palette=[('banner', 'white', 'black'), ('reversed', 'standout', '')]).run()
+    draw_menu(title, menu_body, footer)
 
 
-if os.name == 'nt':
-    os.system('cls')
-else:
-    os.system('clear')
+def describe_instances(nil):
+    instances = ec2.list_instances()
+    title = u'Instances in reservation'
+    for inst in instances:
+        layout = urwid.Text([
+            ('bold result', u'Instance ID:'), inst['InstanceId'],
+        ])
+        body = urwid.Overlay(urwid.Filler(layout, valign='top', top=1, bottom=1),
+                         urwid.SolidFill(u' '), align='left', width=('relative', 50),
+                         valign='top', height=('relative', 50),)
+
+        footer = urwid.Text([u'Press "Enter" to continue'])
+
+        draw_result(title, body, footer)
+        input('')
+    ec2_instances(None)
+
+
+
+def menu_s3():
+    pass
+
+
+def init_main_menu():
+
+    menu_body = [
+        (u'EC2 - Elastic Cloud Computing', menu_ec2),
+        (u'S3 - Simple Storage Service', menu_s3),
+        (u'Exit', exit_program)
+        ]
+    title = urwid.AttrMap(urwid.Text('SCAWS Main Menu', align='center'), 'titlebar')
+
+    footer = urwid.Text([u'Press (', ('quit button', u'Q'), u') to quit.'])
+
+    body = [urwid.Divider()]
+    for x in menu_body:
+        button = urwid.Button(x[0])
+        if x[0] == 'Exit' or x[0] == 'Back':
+            body.append(urwid.Divider())
+        urwid.connect_signal(button, 'click', x[1])
+        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
+
+    body = urwid.Overlay(urwid.ListBox(urwid.SimpleFocusListWalker(body)),
+                         urwid.SolidFill(u' '), align='center', width=('relative', 50),
+                         valign='middle', height=('relative', 50),)
+
+    return urwid.Frame(header=title, body=body, footer=footer)
+
+palette = [
+    ('titlebar', 'black,bold', 'white'),
+    ('quit button', 'dark red,bold', 'black'),
+    ('bold result', 'black,bold', 'white'),
+]
+
+layout = init_main_menu()
+main_loop = urwid.MainLoop(layout, palette, unhandled_input=handle_input)
+main_loop.run()

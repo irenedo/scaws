2efaebaec9e8ceb4d8499dfa83d1430dee987e31
Iván Renedo <renedo@gmail.com>
2015-10-22 22:32:24 +0200

    more info in instances' list

--- scaws.py
+++ scaws.py
@@ -54,7 +54,7 @@ def footer_instance(started):
 
     row3 = urwid.Pile([option5, option6])
 
-    footer = urwid.Columns([row1, row2, row3])
+    footer = urwid.Columns([(30, row1), (30, row2), (30, row3)], min_width=30)
 
     footer = urwid.Pile([footer, urwid.Text(('bold result', u'Spacebar to continue'), align='center')])
 
@@ -80,9 +80,9 @@ def draw_menu(m):
 
 def draw_result(men):
     main_loop.widget.header = urwid.AttrMap(urwid.Text(men.header, align='center'), 'titlebar')
-    main_loop.widget.body = urwid.LineBox(urwid.Overlay(urwid.Filler(urwid.Text(men.body)),
+    main_loop.widget.body = urwid.LineBox(urwid.Overlay(urwid.Filler(men.body, 'top'),
                                                         urwid.SolidFill(u' '), align='center',
-                                          width=('relative', 50), valign='middle', height=('relative', 50)))
+                                          width=('relative', 100), valign='top', height=('relative', 100)))
     main_loop.widget.footer = men.footer
     main_loop.draw_screen()
 
@@ -105,7 +105,7 @@ def menu_back(nil):
 
 def draw_instance_in_menu(inst):
     unavailable = False
-
+    row_width = 15
     started = False
     if inst['State']['Name'] == 'running':
         color = 'started'
@@ -118,17 +118,57 @@ def draw_instance_in_menu(inst):
         unavailable = True
 
     status = inst['State']['Name'].title()
-    menu.body = [
-            ('bold result', u'Instance ID: '), inst['InstanceId'], '\n',
-            ('bold result', u'Instance Status: '), (color, status), '\n',
-            ('bold result', u'PublicDnsName: '), inst['PublicDnsName'], '\n',
-            ('bold result', u'Private Address: '), inst['PrivateIpAddress'], '\n',
-            ('bold result', u'Availability Zone: '), inst['Placement']['AvailabilityZone'], '\n',
-            ('bold result', u'VPC ID: '), inst['VpcId'], '\n',
-            ('bold result', u'Instance Type: '), inst['InstanceType'], '\n',
-            ('bold result', u'Image Id: '), inst['ImageId'], '\n',
-            ('bold result', u'Key Name: '), inst['KeyName'], '\n'
+    first_screen =[
+            ('bold result', u'Instance ID: '), inst['InstanceId'], u'\n',
+            ('bold result', u'Instance Status: '), (color, status), u'\n',
+            ('bold result', u'Public DNS: '), inst['PublicDnsName'], u'\n',
+            ('bold result', u'Private Address: '), inst['PrivateIpAddress'], u'\n',
+            ('bold result', u'Private DNS: '), inst['PrivateDnsName'], u'\n',
+            ('bold result', u'Availability Zone: '), inst['Placement']['AvailabilityZone'], u'\n',
+            ('bold result', u'VPC ID: '), inst['VpcId'], u'\n',
+            ('bold result', u'Subnet ID: '), inst['SubnetId'], u'\n',
+            ('bold result', u'Instance Type: '), inst['InstanceType'], u'\n',
+            ('bold result', u'Image Id: '), inst['ImageId'], u'\n',
+            ('bold result', u'IArchitecture '), inst['Architecture'], u'\n',
+            ('bold result', u'Root Device Type: '), inst['RootDeviceType'], u'\n',
+            ('bold result', u'EBS Optimized: '), str(inst['EbsOptimized']), u'\n',
+            ('bold result', u'Key Name: '), inst['KeyName'],
     ]
+
+    first_screen = urwid.Text(first_screen)
+
+    second_screen = list()
+
+    security_groups = list()
+    security_groups.append([('reversed', u'Security Groups '), u'\n'])
+    security_groups.append([('bold result', u'Group ID       '), ('bold result', u'Name'), u'\n'])
+    for sg in inst['SecurityGroups']:
+        chars = row_width - len(sg['GroupId'])
+        wl = ' ' * chars
+        security_groups.append([sg['GroupId'], wl, sg['GroupName'], u'\n'])
+    second_screen.append((35, urwid.LineBox(urwid.Text(security_groups))))
+
+    tags = list()
+    tags.append([('reversed', u'Tags '), u'\n'])
+    tags.append([('bold result', u'Name           '), ('bold result', u'Value'), u'\n'])
+    for tag in inst['Tags']:
+        chars = row_width - len(tag['Key'])
+        wl = ' ' * chars
+        tags.append([tag['Key'], wl, tag['Value'], u'\n'])
+    second_screen.append((35, urwid.LineBox(urwid.Text(tags))))
+
+    devices = list()
+    devices.append([('reversed', u'Block Device Mappings '), u'\n'])
+    for dev in inst['BlockDeviceMappings']:
+        devices.append([('small title', dev['DeviceName']), u'\n'])
+        devices.append([('bold result', u'Delete On Term: '), str(dev['Ebs']['DeleteOnTermination']), u'\n'])
+        devices.append([('bold result', u'Volume ID: '), dev['Ebs']['VolumeId'], u'\n'])
+        devices.append([('bold result', u'Status: '), dev['Ebs']['Status'], u'\n'])
+    second_screen.append((35, urwid.LineBox(urwid.Text(devices))))
+
+    menu.body = urwid.Pile([urwid.Columns([(60, urwid.LineBox(first_screen))], min_width=60),
+                            urwid.Columns(second_screen, min_width=35)])
+
     menu.footer = footer_instance(started)
     draw_result(menu)
 
@@ -221,10 +261,6 @@ def describe_instances(nil):
     ec2client = ec2.open_client()
     instances = ec2.list_instances(ec2client)
     menu.header = u'Instances in reservation'
-
-    unavailable = False
-    started = False
-
     for inst in instances:
 
         started, unavailable = draw_instance_in_menu(inst)
@@ -254,7 +290,7 @@ def describe_instances(nil):
                 inst = ec2.get_instance(ec2client, inst['InstanceId'])
                 started, unavailable = draw_instance_in_menu(inst)
                 next_instance = False
-            elif ret = 't':
+            elif ret == 't':
                 pass
             else:
                 next_instance = False
@@ -296,7 +332,8 @@ palette = [
     ('stopped', 'dark red,bold', 'white'),
     ('pending', 'dark blue,bold', 'white'),
     ('started', 'dark green', 'white'),
-    ('help', 'dark blue', 'white')
+    ('small title', 'dark blue,bold', 'white'),
+    ('reversed', 'standout', '')
 ]
 menu = Menu
 
